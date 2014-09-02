2babcaa5ba15a3ca43fe3bb8035d382c1ef715a7
Adam Shinn <adam.b.shinn@gmail.com>
2014-09-02 10:30:58 -0700

    re-name clunky variable names

--- coding/closure.py
+++ coding/closure.py
@@ -6,7 +6,7 @@ Write a function which takes a list of strings, return True if "<{[()]}>" are cl
 the string, False if otherwise.
 """
 
-def closure(input_string):
+def closure(string):
     """
     INPUT: string
     OUTPUT: boolean
@@ -14,14 +14,14 @@ def closure(input_string):
     start = "<{[("
     end =   ">}])"
 
-    input_list = list(input_string)
-    for char in input_string:
+    char_list = list(string)
+    for char in string:
         if char in start:
             endchar = end[start.index(char)]
-            if endchar in input_list:
-                input_list.remove(char)
-                input_list.remove(endchar)
-    if input_list:
+            if endchar in char_list:
+                char_list.remove(char)
+                char_list.remove(endchar)
+    if char_list:
         return False
     else:
         return True
