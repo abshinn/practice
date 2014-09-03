6773e2ccdf9cc3de54dc29747596d999c70a0067
Adam Shinn <adam.b.shinn@gmail.com>
2014-09-03 07:38:04 -0700

    use doc string instead of comments

--- coding/applestock.py
+++ coding/applestock.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python2.7 -B -tt
-# Apple Stock
-# 
-# I have an array stockPricesYesterday where:
-# 
-#     The indices are the time, as a number of minutes past trade opening time, which was 9:30am local time.
-#     The values are the price of Apple stock at that time, in dollars.
-#     For example, the stock cost $500 at 10:30am, so stockPricesYesterday[60] = 500.
-#     Write an efficient algorithm for computing the best profit I could have made from 1 purchase and 1 sale 
-#     of 1 Apple stock yesterday. For this problem, we won't allow "shorting"-you must buy before you sell.
+"""
+Apple Stock
+
+I have an array stock_prices where:
+
+    The indices are the time, as a number of minutes past trade opening time, which was 9:30am local time.
+    The values are the price of Apple stock at that time, in dollars.
+    For example, the stock cost $500 at 10:30am, so stockPricesYesterday[60] = 500.
+    Write an efficient algorithm for computing the best profit I could have made from 1 purchase and 1 sale 
+    of 1 Apple stock yesterday. For this problem, we won't allow "shorting"-you must buy before you sell.
+"""
 
 def profit(stock_prices):
     """
