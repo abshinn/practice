a2a982d0ee218bfbe670b3e91d278eccb76cf03d
Adam Shinn <adam.b.shinn@gmail.com>
2014-09-13 21:46:26 -0700

    test within name/main block

--- coding/pascal.py
+++ coding/pascal.py
@@ -3,13 +3,13 @@
 # 
 # Implement pascal's triangle. Given an integer index, return the corresponding row of Pascal's Triangle.
 
-def pascal(ind):
+def pascal(index):
     """
     INPUT: integer index
     OUTPUT: list of integers corresponding to a row of Pascal's Triangle
     """
     p = [1]
-    for _ in xrange(ind):
+    for _ in xrange(index):
         p_a = p + [0]
         p_b = [0] + p
         p = []
@@ -17,12 +17,17 @@ def pascal(ind):
            p.append(p_a[i] + p_b[i])
     return p
 
+
 def test_pascal():
     assert pascal(0) == [1]
     assert pascal(5) == [1, 5, 10, 10, 5, 1]
     assert pascal(10) == [1, 10, 45, 120, 210, 252, 210, 120, 45, 10, 1]
+    print "pascal OK"
 
 
 if __name__ == "__main__":
-    ind = int(raw_input())
-    print pascal(ind)
+    test_pascal()
+
+    for index in xrange(5):
+        print pascal(index)
+   
