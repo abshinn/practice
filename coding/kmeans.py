cb6974d933815c7366246e9da6535ec1d547062e
Adam Shinn <adam.b.shinn@gmail.com>
2014-09-30 21:30:49 -0700

    add data assignment method; add centroid plot method

--- coding/kmeans.py
+++ coding/kmeans.py
@@ -4,7 +4,8 @@ Implement Kmeans using Lloyd's algorithm.
 """
 
 import numpy as np
-np.random
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class KMeans(object):
 
@@ -15,16 +16,51 @@ class KMeans(object):
         """pick random data points as inital cluster centroids"""
         self.centroids = X[np.random.choice(range(self.m), self.n_clusters, replace=False),:]
 
-    def _assign_centroids(self, X):
+    def _assign_data_to_centroids(self, X):
         """assign data points to current centroids"""
-        pass
+
+        self.label = np.zeros((self.m, self.n_clusters))
+
+        for i, example in enumerate(X):
+            self.label[i] = np.argmin( [np.linalg.norm(example - centroid) for centroid in self.centroids] )
 
     def _update_centroids(self):
         """update centroid positions based on current assignment"""
         pass
 
+    def _has_converged(self):
+        return True
+
+    def plot_centroids(self, p):
+        for cluster in self.centroids: 
+            print cluster[0], cluster[1]
+            p.plot(cluster[0], cluster[1], marker="x", markerfacecolor="red", markeredgewidth=4, markersize=10, alpha=.7)
+
     def fit(self, X):
+        """
+        INPUT: X -- numpy.ndarray feature matrix
+        """
         self.m, self.n = X.shape
+        self._initialize_centroids(X)
+        self._assign_data_to_centroids(X)
+
+#         while self._has_converged(): 
+#             pass
+
+
+def sample_set():
+    x = np.linspace(0, 10)
+    y = x + 3 * np.random.randn(50)
+    X = np.c_[x, y]
+    return X
+
 
 if __name__ == "__main__":
-    pass
+    X = sample_set()
+    plt.scatter(X[:,0], X[:,1], marker="o", color="steelblue")
+
+    km = KMeans(n_clusters=3)
+    km.fit(X)
+    km.plot_centroids(plt)
+
+    plt.show()
