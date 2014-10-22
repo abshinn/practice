9618e46754655ba02a811aa8f44ac9428c897c22
Adam Shinn <adam.b.shinn@gmail.com>
2014-10-21 22:30:05 -0700

    pepify

--- machinelearning/kmeans.py
+++ machinelearning/kmeans.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python2.7 -B -tt
-"""
-Implement Kmeans using Lloyd's algorithm.
-"""
+""" Implement Kmeans using Lloyd's algorithm. """
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,13 +12,13 @@ class KMeans(object):
         self.n_clusters = n_clusters
 
     def _initialize_centroids(self, X):
-        """pick random data points as inital cluster centroids"""
+        """ Pick random data points as inital cluster centroids. """
 
         self.old_label, self.label = None, np.zeros(self.m)
         self.centroids = X[np.random.choice(range(self.m), self.n_clusters, replace=False),:]
 
     def _assign_data_to_centroids(self, X):
-        """assign data points to current centroids"""
+        """ Assign data points to current centroids. """
 
         self.old_label, self.label = self.label, np.zeros(self.m)
 
@@ -28,7 +26,7 @@ class KMeans(object):
             self.label[i] = np.argmin( [np.linalg.norm(example - centroid) for centroid in self.centroids] )
 
     def _update_centroids(self, X):
-        """update centroid positions based on current assignment"""
+        """ Update centroid positions based on current assignment. """
 
         self.centroids = np.zeros((self.n_clusters, self.n))
         for k in xrange(self.n_clusters):
@@ -38,13 +36,13 @@ class KMeans(object):
         return not np.all(self.old_label == self.label)
 
     def plot_centroids(self, p):
-        """plot centroids for a two-dimensional feature space"""
+        """ Plot centroids for a two-dimensional feature space. """
 
         for cluster in self.centroids: 
             p.plot(cluster[0], cluster[1], marker="x", markerfacecolor="red", markeredgewidth=4, markersize=10, alpha=0.7)
 
     def plot_residuals(self, p, X):
-        """plot residual from data point to corresponding centroid for a two-dimensional feature space"""
+        """ Plot residual from data point to corresponding centroid for a two-dimensional feature space. """
 
         for k, centroid in enumerate(self.centroids):
             U = X[self.label == k,:]
@@ -52,9 +50,10 @@ class KMeans(object):
                 p.plot([u[0], centroid[0]], [u[1], centroid[1]], linewidth=2, color="black", alpha=0.1)
 
     def fit(self, X):
-        """find k clusters
+        """ Find k clusters.
 
-        INPUT: X -- numpy.ndarray feature matrix
+        INPUT:
+            X -- numpy.ndarray feature matrix
         """
         self.m, self.n = X.shape
         self._initialize_centroids(X)
@@ -68,7 +67,7 @@ class KMeans(object):
 
 
 def sample_set():
-    """generate positively correlated non-seperable data"""
+    """ Generate positively correlated non-seperable data. """
 
     x = np.linspace(0, 10)
     y = x + 3 * np.random.randn(50)
@@ -77,7 +76,7 @@ def sample_set():
 
 
 def example():
-    """example fitting 3 clusters to non-separable data"""
+    """ Example fitting 3 clusters to non-separable data. """
 
     X = sample_set()
     plt.scatter(X[:,0], X[:,1], marker="o", color="steelblue")
@@ -92,3 +91,4 @@ def example():
 
 if __name__ == "__main__":
     example()
+
