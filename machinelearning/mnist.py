82f2b45f56cb66ef066912964a6d517b87d62eb6
Adam Shinn <adam.b.shinn@gmail.com>
2014-10-31 23:40:11 -0700

    load and unpack mnist data

--- machinelearning/mnist.py
+++ machinelearning/mnist.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python2.7 -B -tt
 """ Employ a neural network to classify letters in the MNIST data set. """
 
+import cPickle
+import gzip
+import numpy as np
+
 def download_data():
     """ Download data with wget. """
 
@@ -8,5 +12,12 @@ def download_data():
     os.system("mkdir -p DATA/mnist/")
     os.system("wget {} -P DATA/mnist/".format(url))
 
+def load_mnist():
+    """ Load and unpickle mnist data set. """
+
+    with gzip.open("mnist.pkl.gz", "rb") as f:
+        train_set, valid_set, test_set = cPickle.load(f)
+
 if __name__ == "__main__":
-    pass
+    load_mnist()
+
