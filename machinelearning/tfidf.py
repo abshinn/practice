426215a75e19081ebe2f7de3a8e45336c1ac296d
Adam Shinn <adam.b.shinn@gmail.com>
2014-11-07 17:30:20 -0800

    map out TFIDF class and preparation func

--- machinelearning/tfidf.py
+++ machinelearning/tfidf.py
@@ -3,6 +3,7 @@
 Simple Implementation of Term Frequency - Inverse Document Frequency
 """
 
+import os
 
 def download_mininewsgroups():
     """ Download data with wget and extract with tar. """
@@ -12,6 +13,25 @@ def download_mininewsgroups():
     os.system("wget {} -P DATA/twenty_newsgroups/".format(baseurl + "mini_newsgroups.tar.gz"))
     os.system("tar xvf DATA/twenty_newsgroups/mini_newsgroups.tar.gz")
     os.system("mv mini_newsgroups/ DATA/twenty_newsgroups/")
+
+def prepare_newsgroups():
+    """ Turn newsgroups into a collection of documents per newsgroup. """
+
+    if os.path.isfile("DATA/twenty_newsgroups/mini_newsgroups/talk.religion.misc/84567") == False:
+        download_mininewsgroups()
+
+    newsgroups = os.listdir("DATA/twenty_newsgroups/mini_newsgroups/")
+
+    for newsgroup in newsgroups:
+        pass
+
+
+class TFIDF(object):
+    """ TFIDF Vectorizer. """
+
+    def __init__(self, corpus):
+        self.corpus = corpus
+
 
 if __name__ == "__main__":
     download_mininewsgroups()
