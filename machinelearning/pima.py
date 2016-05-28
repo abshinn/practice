e9b3f802c8cc6e7d6b9c4a8a78189f06ea7b66f9
Adam Shinn <adam.b.shinn@gmail.com>
2016-05-28 11:34:37 -0700

    Update and lintify pima project; add note about using inside venv.

--- machinelearning/pima.py
+++ machinelearning/pima.py
@@ -7,6 +7,9 @@
     examples: 768
 feature type: numerical
         task: classification, clustering
+
+Note: If inside a virtualenv, run Pima class within a jupyter notebook in order
+      to get around Matplotlib Framework issues.
 """
 
 import os
@@ -15,7 +18,9 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.axes_style("darkgrid")
+sns.set_style("whitegrid")
+sns.set_context("notebook", font_scale=1.5,
+                rc={"lines.linewidth": 2.5})
 
 from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import cross_val_score
@@ -28,16 +33,16 @@ from sklearn.grid_search import GridSearchCV
 
 from sklearn.preprocessing import Normalizer
 from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
 
 
 def download_data():
     """ Download data with wget. """
 
-    baseurl = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes"
+    baseurl = ("http://archive.ics.uci.edu/ml/"
+               "machine-learning-databases/"
+               "pima-indians-diabetes/pima-indians-diabetes")
     os.system("mkdir -p DATA/pima/")
     os.system("wget {} -P DATA/pima/".format(baseurl + ".names"))
     os.system("wget {} -P DATA/pima/".format(baseurl + ".data"))
@@ -58,12 +63,14 @@ class Pima(object):
         if os.path.isfile(filepath) == False:
             download_data()
 
-        names = ["n_preg", "plasma", "blood_pressure", "triceps", "insulin", "bmi", "dpf", "age", "label"]
+        column_names = ["n_preg", "plasma", "blood_pressure", "triceps",
+                        "insulin", "bmi", "dpf", "age", "label"]
 
-        data = pd.read_csv(filepath, header=None, names=names)
+        data = pd.read_csv(filepath, header=None, names=column_names)
 
-        data.blood_pressure = data.blood_pressure.apply(lambda x: x if x else np.nan)
-        data.bmi = data.bmi.apply(lambda x: x if x else np.nan)
+        data.blood_pressure = data['blood_pressure']\
+            .apply(lambda x: x if x else np.nan)
+        data.bmi = data['bmi'].apply(lambda x: x if x else np.nan)
         data = data.dropna()
 
         label = data.pop("label")
@@ -71,7 +78,9 @@ class Pima(object):
         return data, label
 
     def train(self, n_examples=None):
-        """ Train and fit data with self.estimator and show classification report. """
+        """
+        Train and fit data with self.estimator and show classification report.
+        """
 
         X = self.data.values.astype(np.float32)
         y = self.label.values
@@ -81,18 +90,22 @@ class Pima(object):
         self.estimator.fit(X_train, y_train)
 
         y_pred = self.estimator.predict(X_test)
-        print classification_report(y_test, y_pred, target_names=["Not Diabetic", "Diabetic"])
+        print classification_report(y_test, y_pred,
+                                    target_names=["Not Diabetic", "Diabetic"])
 
         y_score = self.estimator.predict_proba(X_test)
-        print "roc: {}".format( roc_auc_score(y_test, y_score[:,1]) )
+        print "roc: {}".format(roc_auc_score(y_test, y_score[:, 1]))
 
     def grid_search(self, param_grid, scoring=None, cv=3):
-        """ Run a grid-search with self.estimator through param_grid parameter space. """
+        """
+        Run grid-search with self.estimator through param_grid parameter space.
+        """
 
         X = self.data.values.astype(np.float)
         y = self.label.values
 
-        grid_clf = GridSearchCV(self.estimator, param_grid, scoring=scoring, cv=cv, n_jobs=-1)
+        grid_clf = GridSearchCV(self.estimator, param_grid,
+                                scoring=scoring, cv=cv, n_jobs=-1)
         grid_clf.fit(X, y)
 
         print grid_clf.best_params_
@@ -100,8 +113,12 @@ class Pima(object):
 
         self.estimator = grid_clf.best_estimator_
 
-    def experience_curve(self, train_sizes=None, cv=3, ylim=None, scoring="roc_auc"):
-        """ Return matplotlib plt object with learning/experience curve using self.estimator. """
+    def experience_curve(self, train_sizes=None, cv=3, ylim=None,
+                         scoring="roc_auc"):
+        """
+        Return matplotlib plt object with learning/experience curve using
+        self.estimator.
+        """
 
         X = self.data.values.astype(np.float32)
         y = self.label.values
@@ -109,14 +126,15 @@ class Pima(object):
         if not train_sizes:
             train_sizes = np.linspace(.1, 1.0, 10)
 
-        plt.figure()
+        plt.figure(figsize=(8, 6))
         plt.title("Pima")
         if ylim is not None:
             plt.ylim(*ylim)
         plt.xlabel("Training examples")
         plt.ylabel("Score")
         train_sizes, train_scores, test_scores = learning_curve(
-            self.estimator, X, y, cv=cv, n_jobs=-1, train_sizes=train_sizes, scoring=scoring)
+            self.estimator, X, y, cv=cv, n_jobs=-1, train_sizes=train_sizes,
+            scoring=scoring)
 
         train_scores_mean = np.mean(train_scores, axis=1)
         train_scores_std = np.std(train_scores, axis=1)
@@ -127,7 +145,8 @@ class Pima(object):
                          train_scores_mean + train_scores_std, alpha=0.1,
                          color="r")
         plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1, color="g")
+                         test_scores_mean + test_scores_std,
+                         alpha=0.1, color="g")
         plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
                  label="Training score")
         plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
@@ -145,12 +164,13 @@ class Pima(object):
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-        probas_ = estimator.fit(X_train, y_train).predict_proba(X_test)
+        probas_ = self.estimator.fit(X_train, y_train).predict_proba(X_test)
 
-        fpr, tpr, thresholds = roc_curve(y_test, probas_[:,1])
+        fpr, tpr, thresholds = roc_curve(y_test, probas_[:, 1])
         roc_auc = auc(fpr, tpr)
         print "roc: {}".format(roc_auc)
 
+        plt.figure(figsize=(6, 6))
         plt.plot(fpr, tpr, label="ROC curve (area = {})".format(roc_auc))
         plt.plot([0, 1], [0, 1], 'k--')
         plt.xlim([0.0, 1.0])
@@ -176,12 +196,14 @@ class Pima(object):
         return reducer.fit_transform(Xnorm)
 
     def plot(self):
-        """ Return matplotlib plt object of scatter plot using first two features. """
+        """
+        Return matplotlib plt object of scatter plot using first two features.
+        """
 
         X = self.reduce_dimension(n_components=None)
 
         plt.figure()
-        plt.scatter(X[:,0], X[:,1])
+        plt.scatter(X[:, 0], X[:, 1])
 
         return plt
 
@@ -189,37 +211,39 @@ class Pima(object):
         """ Cluster data with reduced dimensions. """
 
         X = self.reduce_dimension(n_components=2)
-    
         cv = cross_val_score(self.estimator, X, cv=5)
         print cv
         print "mean: {}\nstd: {}".format(np.mean(cv), np.std(cv))
 
-def forest_search():
-    """ Random Forest Grid Search. """
 
-    estimator = RandomForestClassifier()
-    parameters = {"n_estimators":(10, 20, 30, 40), 
-                  "max_features":(.1, .2, .3, "sqrt", "log2"),
-                  "max_depth":(5, 8, 10, 12, 14, 16),
-                  "min_samples_split":(2 ,3, 4),
-                  "min_samples_leaf":(1, 2, 3),
-                 }
+def forest_example():
+    """ Classify using Random Forest. """
 
-    pima.grid_search(param_grid=parameters, scoring=None, cv=5)
+    seed = np.random.randint(2**16)
+    print "seed: {}".format(seed)
+
+    estimator = RandomForestClassifier(max_features=0.3, min_samples_split=2,
+                                       n_estimators=32, max_depth=3,
+                                       min_samples_leaf=1, random_state=seed)
+
+    pima = Pima(estimator)
     pima.train()
     pima.experience_curve().show()
     pima.plot_roc_curve().show()
 
 
+def forest_search_example():
+    """ Random Forest grid search example. """
 
-if __name__ == "__main__":
-    seed = np.random.randint(2**16)
-    print "seed: {}".format(seed)
+    estimator = RandomForestClassifier()
+    parameters = {"n_estimators": (10, 20, 30, 40),
+                  "max_features": (.1, .2, .3, "sqrt", "log2"),
+                  "max_depth": (5, 8, 10, 12, 14, 16),
+                  "min_samples_split": (2, 3, 4),
+                  "min_samples_leaf": (1, 2, 3),
+                 }
 
-    estimator = RandomForestClassifier(max_features=0.3, min_samples_split=2, n_estimators=32, 
-                                       max_depth=3, min_samples_leaf=1, random_state=seed)
-
-    pima = Pima(estimator)
+    pima.grid_search(param_grid=parameters, scoring=None, cv=5)
     pima.train()
     pima.experience_curve().show()
     pima.plot_roc_curve().show()
